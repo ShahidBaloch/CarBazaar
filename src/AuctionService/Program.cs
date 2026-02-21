c1ed2f8e54dd7dddacb30e91e9543d13980442df
Shahid <muhammadshahid6528@gmail.com>
2026-02-21 20:53:12 +0500

    Create the AcutionService , Add entities, dtos and automapper

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,18 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(cfg => { }, AppDomain.CurrentDomain.GetAssemblies());
 
 
 var app = builder.Build();
@@ -13,5 +23,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error initializing database: {ex.Message}");
+}
 
 app.Run();
